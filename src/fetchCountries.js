28cca70c3e75a7b935ebed1a344a7f44c9fa4f3b
AH400796 <A.Hokhman@gmail.com>
2022-11-19 20:08:19 +0200

    commit finally

---
+++ src/fetchCountries.js
@@ -0,0 +1,39 @@
+import {
+  makeMarkupForCountriesList,
+  makeMarkupForCountryCard,
+  makeCountriesList,
+  makeCountryCard,
+  clearPage,
+} from './serviceFunctions';
+import { searchingCountry } from './index';
+import Notiflix from 'notiflix';
+
+export const fetchCountries = function (name) {
+  if (searchingCountry.length < 1) {
+    clearPage();
+    return;
+  }
+  fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        clearPage();
+      } else if (data.length > 1) {
+        makeCountriesList();
+        makeMarkupForCountriesList(data);
+      } else if (data.length === 1) {
+        makeCountryCard();
+        makeMarkupForCountryCard(data);
+      }
+    })
+    .catch(() => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      clearPage();
+    });
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+let searchingCountry = null;
+const { inputCountry, countryList, countryInfoCard } = {
+  inputCountry: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfoCard: document.querySelector('.country-info'),
+};
+
+inputCountry.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(e) {
+  searchingCountry = e.target.value.trim();
+  if (searchingCountry.length < 1) {
+    countryList.innerHTML = '';
+  }
+  fetchCountries(searchingCountry);
+}
+
+export { countryList, countryInfoCard, searchingCountry };
